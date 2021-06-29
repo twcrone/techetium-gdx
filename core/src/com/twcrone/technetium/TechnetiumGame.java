55c19ef5e632a1841c9d6c08be74d536b48dfa77
Todd W Crone <twcrone@gmail.com>
2021-06-29 18:01:04 -0400

    Replace resources and finish logic

--- core/src/com/twcrone/technetium/TechnetiumGame.java
+++ core/src/com/twcrone/technetium/TechnetiumGame.java
@@ -8,11 +8,17 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
+import java.util.Iterator;
 
 public class TechnetiumGame extends ApplicationAdapter {
+	private Array<Rectangle> raindrops;
+	private long lastDropTime;
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
 	private Rectangle bucket;
@@ -44,17 +50,16 @@ public class TechnetiumGame extends ApplicationAdapter {
 
 		rainMusic.setLooping(true);
 		rainMusic.play();
+
+		raindrops = new Array<>();
+		spawnRaindrop();
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0.2f, 1);
 		camera.update();
 
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		batch.draw(bucketImage, bucket.x, bucket.y);
-		batch.end();
 
 		if(Gdx.input.isTouched()) {
 			Vector3 touchPos = new Vector3();
@@ -67,11 +72,46 @@ public class TechnetiumGame extends ApplicationAdapter {
 		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
 		if(bucket.x < 0) bucket.x = 0;
 		if(bucket.x > 800 - 64) bucket.x = 800 - 64;
+
+		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
+
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(bucketImage, bucket.x, bucket.y);
+		for(Rectangle raindrop: raindrops) {
+			batch.draw(dropImage, raindrop.x, raindrop.y);
+		}
+		batch.end();
+
+		for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext(); ) {
+			Rectangle raindrop = iter.next();
+			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+			if(raindrop.y + 64 < 0) iter.remove();
+			if(raindrop.overlaps(bucket)) {
+				dropSound.play();
+				iter.remove();
+			}
+		}
+
+
+	}
+
+	private void spawnRaindrop() {
+		Rectangle raindrop = new Rectangle();
+		raindrop.x = MathUtils.random(0, 800-64);
+		raindrop.y = 480;
+		raindrop.width = 64;
+		raindrop.height = 64;
+		raindrops.add(raindrop);
+		lastDropTime = TimeUtils.nanoTime();
 	}
 	
 	@Override
 	public void dispose () {
-//		batch.dispose();
-//		img.dispose();
+		dropImage.dispose();
+		bucketImage.dispose();
+		dropSound.dispose();
+		rainMusic.dispose();
+		batch.dispose();
 	}
 }
