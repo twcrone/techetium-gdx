196bb94fd1743e03bd22bec7b3fbd02135dda10f
Todd W Crone <twcrone@gmail.com>
2021-06-29 17:39:54 -0400

    Minor updates

--- core/src/com/twcrone/technetium/TechnetiumGame.java
+++ core/src/com/twcrone/technetium/TechnetiumGame.java
@@ -4,10 +4,17 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class TechnetiumGame extends ApplicationAdapter {
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Rectangle bucket;
+
 	private Texture dropImage;
 	private Texture bucketImage;
 	private Sound dropSound;
@@ -16,6 +23,17 @@ public class TechnetiumGame extends ApplicationAdapter {
 	
 	@Override
 	public void create () {
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
+		batch = new SpriteBatch();
+
+		bucket = new Rectangle();
+		bucket.x = 800 / 2 - 64 / 2;
+		bucket.y = 20;
+		bucket.width = 64;
+		bucket.height = 64;
+
 		dropImage = new Texture(Gdx.files.internal("droplet.png"));
 		bucketImage = new Texture(Gdx.files.internal("bucket.png"));;
 
@@ -29,9 +47,12 @@ public class TechnetiumGame extends ApplicationAdapter {
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
-//		batch.begin();
-//		batch.draw(img, 0, 0);
-//		batch.end();
+		camera.update();
+
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(bucketImage, bucket.x, bucket.y);
+		batch.end();
 	}
 	
 	@Override
